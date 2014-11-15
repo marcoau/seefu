b734fce62750b1f08aa6ddbc92b176cea7315017
Marco Au <marco.au.321@gmail.com>
2014-11-15 11:27:45 +0800

    add login and signup forms

--- client/modules/home/home.js
+++ client/modules/home/home.js
@@ -1,3 +1,16 @@
 angular.module('app.home', [])
 .controller('HomeController', ['$scope', function($scope) {
+  $scope.userLogin = function(user) {
+    console.log(user);
+  };
+  $scope.supplierLogin = function(supplier) {
+    console.log(supplier);
+  };
+  $scope.userSignup = function(user) {
+    console.log(user);
+  };
+  $scope.supplierSignup = function(supplier) {
+    console.log(supplier);
+  };
+
 }]);
